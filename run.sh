8b57ef0be3cd6b09b3737aa09c4d99d480df39d9
Fabi <schyrerf@gmail.com>
2019-09-24 13:49:42 +0200

    sudo

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 # run demo
 
-oc new-project pag-bluegreen --display-name="Blue/Green Deployment Pipeline Demo"
-oc new-app jenkins-persistent --param MEMORY_LIMIT=1Gi --param VOLUME_CAPACITY=2Gi
-oc new-app openshift/deployment-example:v1 --name=example-green
-oc new-app openshift/deployment-example:v2 --name=example-blue
-oc set triggers dc/example-green --remove-all
-oc set triggers dc/example-blue --remove-all
-oc expose svc/example-green --name=example
-oc create -f https://raw.githubusercontent.com/fabianschyrer/pag-openshift-blue-green-example/master/example-pipeline.yaml
+sudo oc new-project pag-bluegreen --display-name="Blue/Green Deployment Pipeline Demo"
+sudo oc new-app jenkins-persistent --param MEMORY_LIMIT=1Gi --param VOLUME_CAPACITY=2Gi
+sudo oc new-app openshift/deployment-example:v1 --name=example-green
+sudo oc new-app openshift/deployment-example:v2 --name=example-blue
+sudo oc set triggers dc/example-green --remove-all
+sudo oc set triggers dc/example-blue --remove-all
+sudo oc expose svc/example-green --name=example
+sudo oc create -f https://raw.githubusercontent.com/fabianschyrer/pag-openshift-blue-green-example/master/example-pipeline.yaml
